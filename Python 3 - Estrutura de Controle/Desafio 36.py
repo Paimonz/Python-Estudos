8d06ead0b6c394a3e5118083f11247c20e472d9f
Paimonz <309440@aluno.unilins.edu.br>
2021-09-06 15:56:32 -0300

    add orientação da elaboração da questão

--- Python 3 - Estrutura de Controle/Desafio 36.py
+++ Python 3 - Estrutura de Controle/Desafio 36.py
@@ -1,3 +1,9 @@
+''' Escreve um programa para aprovar o empréstimo bancário 
+para a compra de uma casa.Perguntando o valor da casa, o 
+salário do comprador e em quantos anos ele vai pagar.
+A prestação mensal, não pode exceder 30% do salário ou 
+então o empréstimo será negado.'''
+
 from time import sleep
 print ('\n')
 print ('*_*'*15)
